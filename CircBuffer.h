370ccf35197a60cde529cff88b0a825a28013d96
WojtiGitHub <39150835+WojtiGitHub@users.noreply.github.com>
2021-09-15 12:51:20 +0200

    Update CircBuffer.h

--- CircBuffer.h
+++ CircBuffer.h
@@ -4,37 +4,23 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* Struktura bufora kołowego, zawiera:
-
-   bufor abuffer
-   wskaźnik zapisu head
-   wskaźnik odczytu tail
-   zmienną rozmiaru bufora
-
- */
+/*
+@brief Circular Buffer structure definition
+*/
 typedef struct{
 
-    uint8_t *abuffer;
-    uint8_t *phead;
-    uint8_t *ptail;
-    uint8_t xbufferSize;
+    uint8_t *buffer;     /*!< Circular buffer buffer >*/
+    uint8_t  bufferSize; /*!< Size of buffer >*/
+    uint8_t *head;       /*!< Circular buffer head >*/
+    uint8_t *tail;       /*!< Circular buffer tail >*/
 
 } CircBuffer;
 
-/* Inicjuje strukturę bufora kołowego (circbuffer) buforem (buffer) o rozmiarze (buffersize) */
-void CircBuffer_init(CircBuffer *circbuffer, uint8_t* const buffer, const uint8_t buffersize);
+/* Initialization function */
+bool CircBuffer_init(CircBuffer * const circbuffer, uint8_t * const buffer, const uint8_t buffersize);
 
-/* Dodaje znak (character) do buforu kołowego pod warunkiem, że jest miejsce
-   Jeśli było miejsce to dodaje znak i zwraca [true]
-   Jeśli nie było miejsca porzuca znak i zwraca [false]
-*/
-bool CircBuffer_addChar(CircBuffer *circbuffer, const uint8_t *character);
-
-/* Ściąga znak z bufora kołowego pod warunkiem, że bufor nie jest pusty
-   Jeśli nie jest pusty to ściąga znak do zmiennej (character) i zwraca [true]
-   Jeśli jest pusty zwraca [false]
-*/
-bool CircBuffer_getChar(CircBuffer *circbuffer, uint8_t *character);
-
+/* Operation functions */
+bool CircBuffer_write(CircBuffer * const circbuffer, const uint8_t * const data, const uint8_t datasize);
+bool CircBuffer_read(CircBuffer * const circbuffer, uint8_t * const data, const uint8_t datasize);
 
 #endif
